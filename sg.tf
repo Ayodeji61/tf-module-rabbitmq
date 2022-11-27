8b383b3df9f25884385459577291e0c3c697247e
Ayodeji61 <alabidejimichael@gmail.com>
2022-11-26 18:16:24 -0700

    Updating

---
+++ sg.tf
@@ -0,0 +1,36 @@
+
+resource "aws_security_group" "sg" {
+  name        = "${var.env}-${var.name}-ec2.sg"
+  description = "${var.env}-${var.name}-ec2.sg"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    description      = "RABBITMQ"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = [var.BASTION_NODE]
+  }
+
+  ingress {
+    description      = "APP"
+    from_port        = 5672
+    to_port          = 5672
+    protocol         = "tcp"
+    cidr_blocks      = [var.vpc_cidr]
+  }
+
+
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "${var.env}-${var.name}-ec2.sg"
+  }
+}
