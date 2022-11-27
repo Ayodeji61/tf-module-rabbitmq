024cf19d3012025c7abdc1afa80a79e0cd8fadab
Ayodeji61 <alabidejimichael@gmail.com>
2022-11-27 12:54:26 -0700

    Updating

--- route53.tf
+++ route53.tf
@@ -1,6 +1,6 @@
 resource "aws_route53_record" "rabbitmq" {
   zone_id = var.private_zone_id
-  name    = "$rabbitmq-${var.env}"
+  name    = "rabbitmq-${var.env}"
   type    = "A"
   ttl     = 300
   records = [aws_instance.rabbitmq.private_ip]
